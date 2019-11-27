cc0d5ef436de49aaec944c0abf666b0f530f372c
paulsegado <46777897+paulsegado@users.noreply.github.com>
2019-11-27 10:19:19 +0100

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,14 @@
 
+const InputText = () => {
+    const inputRef = useRef(null);
+    const onButtonClick = () => {
+
+    inputEl.current.focus();
+    };
+    return (
+    <>
+    <input ref={inputEl} type="text" />
+    <button onClick={onButtonClick}>Donner le focus au champ</button>
+    </>
+    );
+    }
